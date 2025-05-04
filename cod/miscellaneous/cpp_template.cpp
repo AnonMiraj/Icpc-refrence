b52014ab4bb720caf0d10df14ee3ae52757f5c3c
Ramez Medhat <iramezdev@gmail.com>
2025-05-04 17:27:05 +0300

    Editing template, it's now shorter and better.

--- cod/miscellaneous/cpp_template.cpp
+++ cod/miscellaneous/cpp_template.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
-
-#define endl '\n'
-#define int long long
-#define ld  long double
-#define all(a) (a).begin(), (a).end()
-#define sz(a) (int)(a).size()
-#define pb push_back
-#define F first
-#define S second
-#define vi vector<int>
-
 using namespace std;
+
+#define int    long long
+#define vi     vector<int>
+#define pii    pair<int, int>
+#define all(v) v.begin(), v.end()
+
 
 void file() {
   freopen("input.in", "r", stdin);
@@ -18,16 +13,15 @@ void file() {
 }
 
 void Solve() {
+
 }
 
-int32_t main() { 
-  ios_base::sync_with_stdio(false);
+int32_t main() {
+  ios_base::sync_with_stdio(false); 
   cin.tie(nullptr);
   // file();
-  
-  int t = 1; 
+  int t = 1;
   // cin >> t;
-  for(int i = 1; i <= t; ++i) {
-    Solve();
-  }
+  while (t--) Solve();
+  return 0;
 }
