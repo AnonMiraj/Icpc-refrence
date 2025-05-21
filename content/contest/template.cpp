c72fc31d021b8982e164b6cf553238e955248b42
Ramez Medhat <iramezdev@gmail.com>
2025-05-21 19:24:33 +0300

    updating template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,31 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int    long long
+#define vi     vector<int>
+#define all(v) v.begin(), v.end()
 
-#define rep(i, a, b) for (int i = a; i < (b); ++i)
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-#define int long long
-#define uint unsigned long long
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-
-mt19937 rng = mt19937(random_device()());
-
-int rand_int(int a, int b) {
-    return uniform_int_distribution<int>(a, b)(rng);
-}
-
-void solve() {
+void Solve() {
 
 }
 
-int32_t main() {
-  cin.tie(0)->sync_with_stdio(0);
-  cin.exceptions(cin.failbit);
-
-  int t = 1;
-  cin >> t;
-  while (t--) {
-    solve();
-  }
+signed main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    // freopen("file.in", "r", stdin);
+    int t = 1;
+    // cin >> t;
+    while (t--) Solve();
+    return 0;
 }
