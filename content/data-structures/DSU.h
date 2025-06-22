4f8c27b94921040f0b12dd4dee762f8c8ce2dfdb
Ramez Medhat <iramezdev@gmail.com>
2025-06-22 19:15:02 +0300

    Updating DSU

---
+++ content/data-structures/DSU.h
@@ -0,0 +1,31 @@
+/**
+ * Author: Lukas Polacek
+ * Date: 2009-10-26
+ * License: CC0
+ * Source: folklore
+ * Description: Disjoint-set data structure.
+ * Time: $O(\alpha(N))$
+ */
+#pragma once
+
+struct DSU {
+    vector<int> parent, size;
+    int count; // of component
+
+    DSU(int n) : parent(n + 1), size(n + 1, 1), count(n) { iota(all(parent), 0); }
+
+    int find(int i) { return (parent[i] == i ? i : (parent[i] = find(parent[i]))); }
+
+    bool same(int i, int j) { return find(i) == find(j); }
+
+    int getSize(int i) { return size[find(i)]; }
+
+    int merge(int i, int j) {
+        if ((i = find(i)) == (j = find(j))) return -1;
+        else --count;
+        if (size[i] > size[j]) swap(i, j);
+        parent[i] = j;
+        size[j] += size[i];        
+        return j;
+    }
+};
