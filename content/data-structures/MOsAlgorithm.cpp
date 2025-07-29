6a2eaecafbaf01e8d9d5ba73880185f04914e338
Mohamed <mohamed1212213183@gmail.com>
2025-07-29 08:52:56 +0300

    refactor MosAlgo

--- content/data-structures/MOsAlgorithm.cpp
+++ content/data-structures/MOsAlgorithm.cpp
@@ -1,49 +1,61 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define int long long
-#define fastio() ios::sync_with_stdio(0); cin.tie(0);
+#define fastio()                                                               \
+  ios::sync_with_stdio(0);                                                     \
+  cin.tie(0);
 
 struct Query {
-    int l, r, idx;
-    bool operator<(const Query &other) const {
-        int block_size = 450;
-        int block_a = l / block_size;
-        int block_b = other.l / block_size;
-        if (block_a != block_b) return block_a < block_b;
-        return r < other.r;
-    }
+  int l, r, idx;
+  bool
+  operator<(const Query &other) const { // first if they are not starting at the
+                                        // same block then sort with l
+                                        // if they are the same then sort buy r
+    int block_size = 450;
+    int block_a = l / block_size;
+    int block_b = other.l / block_size;
+    if (block_a != block_b)
+      return block_a < block_b;
+    return r < other.r;
+  }
 };
+void solve() {
+  fastio();
+  int n, q;
+  cin >> n >> q;
+  vector<int> a(n);
+  for (auto &x : a)
+    cin >> x;
 
-int32_t main() {
-    fastio();
-    int n, q;
-    cin >> n >> q;
-    vector<int> a(n);
-    for (auto &x : a) cin >> x;
+  vector<Query> queries(q); // store queres to make offline process
+  for (int i = 0; i < q; ++i) {
+    int l, r;
+    cin >> l >> r;
+    queries[i] = {--l, --r, i}; // convert to 0-based
+  }
 
-    vector<Query> queries(q);
-    for (int i = 0; i < q; ++i) {
-        int l, r;
-        cin >> l >> r;
-        queries[i] = {--l, --r, i};  // convert to 0-based
-    }
+  sort(queries.begin(), queries.end()); // sort to make the largest amount of
+                                        // overlaping with logic at struct
 
-    sort(queries.begin(), queries.end());
+  vector<int> freq(1e6 + 5, 0); // store frq numbers
+  int distinct = 0;
+  int l = 0, r = -1;
+  vector<int> ans(q);
 
-    vector<int> freq(1e6 + 5, 0);
-    int distinct = 0;
-    int l = 0, r = -1;
-    vector<int> ans(q);
+  for (auto query :
+       queries) { // shrink and extend logic and calc the new distinct elements
+    while (r < query.r)
+      if (++freq[a[++r]] == 1)
+        ++distinct;
+    while (r > query.r)
+      if (--freq[a[r--]] == 0)
+        --distinct;
+    while (l < query.l)
+      if (--freq[a[l++]] == 0)
+        --distinct;
+    while (l > query.l)
+      if (++freq[a[--l]] == 1)
+        ++distinct;
+    ans[query.idx] = distinct;
+  }
 
-    for (auto query : queries) {
-        while (r < query.r) if (++freq[a[++r]] == 1) ++distinct;
-        while (r > query.r) if (--freq[a[r--]] == 0) --distinct;
-        while (l < query.l) if (--freq[a[l++]] == 0) --distinct;
-        while (l > query.l) if (++freq[a[--l]] == 1) ++distinct;
-        ans[query.idx] = distinct;
-    }
-
-    for (int x : ans)
-        cout << x << '\n';
-    return 0;
+  for (int x : ans)
+    cout << x << '\n';
 }
