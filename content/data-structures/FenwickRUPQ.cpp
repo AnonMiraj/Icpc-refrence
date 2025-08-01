0b6b8a85899fd65da790afee149b23c735e64f55
anonmiraj <nabilmalek48@gmail.com>
2025-08-01 19:58:24 +0300

    fix more stuff

---
+++ content/data-structures/FenwickRUPQ.cpp
@@ -0,0 +1,26 @@
+/**
+ * Author: Ramez Medhat
+ * Description: Range Update, Point Query
+ * Time: O(logN)
+ */
+struct FenwickRUPQ {
+    int n;
+    vi f;
+    FenwickRUPQ(int _n) : n(_n), f(n + 1, 0) {}
+
+    void update(int idx, int val) {
+        for (; idx <= n; idx += idx & -idx)
+            f[idx] += val;
+    }
+
+    void rangeAdd(int l, int r, int val) {
+        update(l, val);
+        if (r + 1 <= n) update(r + 1, -val);
+    }
+
+    int pointQuery(int idx) {
+        int res = 0;
+        for (; idx > 0; idx -= idx & -idx) res += f[idx];
+        return res;
+    }
+};
